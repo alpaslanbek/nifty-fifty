6f2bec00448256ce9bbf41222babc324419d1291
oguz <oguzalkisoglu@gmail.com>
2020-04-06 17:53:33 +0300

    Merge branch 'master' of https://github.com/alpaslanbek/nifty-fifty
     Conflicts:
    	.idea/vcs.xml

---
+++ app/src/main/java/com/example/niftyfifty/GamePlatformActivity.java
@@ -0,0 +1,56 @@
+package com.example.niftyfifty;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+
+public class GamePlatformActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private GridView gridView;
+    private GridAdapter adapter;
+    private List<String> lstGridSource = new ArrayList<>();
+    private Button btn_restart;
+    private List<String> last25NumberList = new ArrayList<String>();
+
+    String[] array_first25Numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"};
+    String[] array_last25Numbers = {"26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_platform);
+
+        btn_restart = (Button)findViewById(R.id.btn_restart);
+        btn_restart.setOnClickListener(this);
+
+        lstGridSource = Arrays.asList(array_first25Numbers);
+        last25NumberList = Arrays.asList(array_last25Numbers);
+
+        Collections.shuffle(lstGridSource);
+        Collections.shuffle(last25NumberList);
+        gridView = (GridView) findViewById(R.id.gridView);
+        //é a classe gridadapter
+        adapter = new GridAdapter(lstGridSource,GamePlatformActivity.this);
+        gridView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btn_restart) {
+            Collections.shuffle(lstGridSource);
+            gridView = (GridView) findViewById(R.id.gridView);
+            //é a classe gridadapter
+            adapter = new GridAdapter(lstGridSource,GamePlatformActivity.this);
+            gridView.setAdapter(adapter);
+        }
+    }
+}
+
--- app/src/main/java/com/example/niftyfifty/MainActivity.java
+++ app/src/main/java/com/example/niftyfifty/MainActivity.java
@@ -2,16 +2,132 @@ package com.example.niftyfifty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    CallbackManager callbackManager = null;
+    AccessToken accessToken = null;
+    Button btn_guest = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v("nifty-fifty", "Deneme 1 2");
+        btn_guest = (Button) findViewById(R.id.btn_guest);
+        btn_guest.setOnClickListener(this);
+        btn_guest.setText("Continue as Guest");
+
+        callbackManager = CallbackManager.Factory.create();
+
+        final LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //Toast.makeText(getApplicationContext(), "SUCCESSFUL LOGIN OGUZ", Toast.LENGTH_SHORT).show();
+                //Map<String, String> facebookUserInfo = getFacebookLoginInformation(loginResult.getAccessToken());
+                final Intent gamePlatformActivity = new Intent(MainActivity.this, GamePlatformActivity.class);
+                accessToken = loginResult.getAccessToken();
+                //String name = facebookUserInfo.get("name").toString();
+                //gamePlatformActivity.putExtra("name", name);
+                //startActivity(gamePlatformActivity);
+                // App code
+                final String name = "";
+                GraphRequest request = GraphRequest.newMeRequest(
+                        accessToken,
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                // Application code
+                                try {
+                                    gamePlatformActivity.putExtra("name", response.getJSONObject().getString("name"));
+                                    //gamePlatformActivity.putExtra("birthday", response.getJSONObject().getString("birthday"));
+                                    //startActivity(gamePlatformActivity);
+                                    Toast.makeText(getApplicationContext(), "SUCCESSFUL LOGIN " + response.getJSONObject().getString("name"), Toast.LENGTH_LONG).show();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name");
+                request.setParameters(parameters);
+                request.executeAsync();
+                //startActivity(gamePlatformActivity);
+
+            }
+            @Override
+            public void onCancel() {
+                // App code
+                Toast.makeText(getApplicationContext(), "CANCELLED LOGIN", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Toast.makeText(getApplicationContext(), "ERRORED LOGIN", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private Map<String, String> getFacebookLoginInformation(AccessToken accessToken) {
+        final Map<String, String> values = new HashMap<String, String>();
+        /**
+         Creating the GraphRequest to fetch user details
+         1st Param - AccessToken
+         2nd Param - Callback (which will be invoked once the request is successful)
+         **/
+        GraphRequest request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+            //OnCompleted is invoked once the GraphRequest is successful
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String name = object.getString("name");
+                    values.put("name", object.getString("name"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return values;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btn_guest) {
+            Intent gamePlatformActivity = new Intent(MainActivity.this, GamePlatformActivity.class);
+            startActivity(gamePlatformActivity);
+        }
     }
 }
