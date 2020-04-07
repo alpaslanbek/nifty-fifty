4a7a120d350c41c7ba15ba6353f425ec51a138fc
oguz <oguzalkisoglu@gmail.com>
2020-04-07 11:25:11 +0300

    Merge branch 'master' of https://github.com/alpaslanbek/nifty-fifty
     Conflicts:
    	.idea/vcs.xml

--- app/src/main/java/com/example/niftyfifty/GamePlatformActivity.java
+++ app/src/main/java/com/example/niftyfifty/GamePlatformActivity.java
@@ -2,8 +2,10 @@ package com.example.niftyfifty;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.GridView;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,6 +21,8 @@ public class GamePlatformActivity extends AppCompatActivity implements View.OnCl
     private List<String> lstGridSource = new ArrayList<>();
     private Button btn_restart;
     private List<String> last25NumberList = new ArrayList<String>();
+    private Chronometer chronometer;
+    public static int totalClickNumber = 0;
 
     String[] array_first25Numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"};
     String[] array_last25Numbers = {"26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50"};
@@ -27,6 +31,8 @@ public class GamePlatformActivity extends AppCompatActivity implements View.OnCl
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_platform);
+
+        chronometer = findViewById(R.id.chronometer);
 
         btn_restart = (Button)findViewById(R.id.btn_restart);
         btn_restart.setOnClickListener(this);
@@ -38,8 +44,9 @@ public class GamePlatformActivity extends AppCompatActivity implements View.OnCl
         Collections.shuffle(last25NumberList);
         gridView = (GridView) findViewById(R.id.gridView);
         //é a classe gridadapter
-        adapter = new GridAdapter(lstGridSource,GamePlatformActivity.this);
+        adapter = new GridAdapter(lstGridSource,GamePlatformActivity.this, last25NumberList, chronometer);
         gridView.setAdapter(adapter);
+
     }
 
     @Override
@@ -48,7 +55,9 @@ public class GamePlatformActivity extends AppCompatActivity implements View.OnCl
             Collections.shuffle(lstGridSource);
             gridView = (GridView) findViewById(R.id.gridView);
             //é a classe gridadapter
-            adapter = new GridAdapter(lstGridSource,GamePlatformActivity.this);
+            chronometer.stop();
+            chronometer.setBase(SystemClock.elapsedRealtime());
+            adapter = new GridAdapter(lstGridSource,GamePlatformActivity.this, last25NumberList, chronometer);
             gridView.setAdapter(adapter);
         }
     }
